3191d98e5d6eb3d5482d36e30d5a12940e868ee9
Lovesth <1667286082@qq.com>
2024-12-01 21:30:05 +0800

    commit

--- Future.h
+++ Future.h
@@ -46,37 +46,37 @@ namespace async_framework
 
     public:
         using value_type = T;
-        Future(FutureState<inner_value_type> *fs) : shareState_(fs)
+        Future(FutureState<inner_value_type> *fs) : sharedState_(fs)
         {
-            if (shareState_)
+            if (sharedState_)
             {
-                shareState_->attachOne();
+                sharedState_->attachOne();
             }
         }
 
-        Future(Try<inner_value_type> &&t) : shareState_(nullptr), localState(std::move(t)) {}
+        Future(Try<inner_value_type> &&t) : sharedState_(nullptr), localState(std::move(t)) {}
 
         ~Future()
         {
-            if (shareState_)
+            if (sharedState_)
             {
-                shareState_->detachOne();
+                sharedState_->detachOne();
             }
         }
 
         Future(const Future &) = delete;
         Future &operator=(const Future &) = delete;
 
-        Future(Future &&other) : shareState_(other.shareState_), localState_(std::move(other.localState_))
+        Future(Future &&other) : sharedState_(other.sharedState_), localState_(std::move(other.localState_))
         {
-            other.shareState_ = nullptr;
+            other.sharedState_ = nullptr;
         }
 
         Future &operator(Future &&other)
         {
             if (this != &other)
             {
-                std::swap(shareState_, other.shareState_);
+                std::swap(sharedState_, other.sharedState_);
                 localState_ = std::move(other.localState_);
             }
             return *this;
@@ -90,12 +90,12 @@ namespace async_framework
     public:
         bool valid() const
         {
-            return shareState_ != nullptr || localState_.hasResult();
+            return sharedState_ != nullptr || localState_.hasResult();
         }
 
         bool hasResult() const
         {
-            return localState_.hasResult() || shareState_->hasResult();
+            return localState_.hasResult() || sharedState_->hasResult();
         }
 
         std::add_rvalue_reference_t<T> value() &&
@@ -120,16 +120,193 @@ namespace async_framework
             return result().value();
         }
 
-        // Try<T>&& result() && requires(!std::is_void_v<T>) {
-        //     return std::move()
-        // }
+        Try<T> &&result() &&
+            requires(!std::is_void_v<T>)
+        {
+            return std::move(getTry(*this));
+        }
+
+        Try<T> &result() &
+            requires(!std::is_void_v<T>)
+        {
+            return getTry(*this);
+        }
+
+        const Try<T> &result() const &
+            requires(!std::is_void_v<T>)
+        {
+            return getTry(*this);
+        }
+
+        Try<void> result() &&
+            requires(std::is_void_v<T>)
+        {
+            return getTry(*this);
+        }
+
+        Try<void> result() &
+            requires(std::is_void_v<T>)
+        {
+            return getTry(*this);
+        }
+
+        Try<void> result() const &
+            requires(std::is_void_v<T>)
+        {
+            return getTry(*this);
+        }
+
+        // get is only allowed on rvalue, aka, Future is not valid after get
+        // invoked.
+        //
+        // Get value blocked thread when the future doesn't have a value.
+        // If future in uthread context, use await(future) to get value without
+        // thread blocked.
+
+        T get() &&
+        {
+            wait();
+            return (std::move(*this)).value();
+        }
+
+        // Implemention for get() to wait synchronously.
+        void wait()
+        {
+            logicAssert(valid(), "Future is broken.");
+            if (hasResult())
+            {
+                return;
+            }
+
+            // wait in the same executor may cause deadlock.
+            assert(!currentThreadInExecutor());
+
+            // the state is a shared state
+            Promise<T> promise;
+            auto future = promise.getFuture();
+
+            // following continuation is simpl, execute inplace
+            sharedState_->setExecutor(nullptr);
+
+            std::mutex mtx;
+            std::condition_variable cv;
+            std::atomic<bool> done{false};
+
+            sharedState_->setContinuation([&mtx, &cv, &done, p = std::move(promise)](Try<T> &t) mutable
+                                          {
+                std::unique_lock<std::mutex> lock(mtx);
+                p.setValue(std::move(t));
+                done.store(true, std::memory_order_relaxed);
+                cv.notify_one(); });
+
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [&done]()
+                    { return done.load(std::memory_order_relaxed); });
+            *this = std::move(future);
+            assert(sharedState_->hasResult());
+        }
+
+        // Set the executor for the future. This only works for rvalue.
+        // So the original future shouldn't be accessed after setting
+        // an executor. The user should use the returned future instead.
+        Future<T> via(Executor *executor) &&
+        {
+            setExecutor(executor);
+            Future<T> ret(std::move(*this));
+            // 注意via函数只能用于右值，因此需要返回一个新的Future<T>
+            return ret;
+        }
+
+        // thenTry() is only allowed on rvalues, do not access a future after
+        // thenTry() called. F is a callback function which takes Try<T>&& as
+        // parameter.
+        //
+        template <typename F, typename R = TryCallableResult<T, F>>
+
+
+    public:
+        // This section is public because they may invoked by other type of Future.
+        // They are not suppose to be public.
+        // FIXME: mark the section as private.
+        void setExecutor(Executor *ex)
+        {
+            if (sharedState_)
+            {
+                sharedState_->setExecutor(ex);
+            }
+            else
+            {
+                localState_.setExecutor(ex);
+            }
+        }
+
+        Executor *getExecutor()
+        {
+            if (sharedState_)
+            {
+                return sharedState_->getExecutor();
+            }
+            else
+            {
+                return localState_.getExecutor();
+            }
+        }
+
+        template <typename F>
+        void setContinuation(F &&func)
+        {
+            assert(valid());
+            if (sharedState_)
+            {
+                sharedState_->setContinuation(std::forward<F>(func));
+            }
+            else
+            {
+                localState_.setContinuation(std::forward<F>(func));
+            }
+        }
+
+        bool currentThreadInExecutor() const
+        {
+            assert(valid());
+            if (sharedState_)
+            {
+                // caller in executor?
+                return sharedState_->currentThreadInExecutor();
+            }
+            else
+            {
+                return localState_.currentThreadInExecutor();
+            }
+        }
+
+        bool TEST_hasLocalState() const
+        {
+            return localState_.hasResult();
+        }
+
     private:
         template <typename T>
-        static decltype(auto) getTry(T& self) {
+        static decltype(auto) getTry(T &self)
+        {
             logicAssert(self.valid(), "Future is broken.");
+            logicAssert(self.localState_.hasResult() || self.sharedState_.hasResult(), "Future is not ready");
+            if (self.sharedState_)
+            {
+                return self.sharedState_->getTry();
+            }
+            else
+            {
+                return self.localState_.getTry();
+            }
         }
+
+        // continuation returns a future
+        template <typename F, typename R>
+        Future<typename R::Return>
+
     private:
-        FutureState<inner_value_type> *shareState_;
+        FutureState<inner_value_type> *sharedState_;
         // Ready-Future does not have a Promise, an inline state is faster.
         LocalState<inner_value_type> localState_;
 

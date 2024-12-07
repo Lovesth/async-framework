0e60f85f3fdb58177ec1d65a71b31100cfea954d
Lovesth <1667286082@qq.com>
2024-12-07 11:50:54 +0800

    commit

--- coro/Lazy.h
+++ coro/Lazy.h
@@ -540,5 +540,11 @@ namespace async_framework
         // should implement `coAwait(Executor*)` member method. Then the caller would
         // pass its executor instance to the awaitable.
 
+        template <typename T>
+        concept isDerivedFromLazyLocal = std::is_base_of_v<LazyLocalBase, T> && requires(const T *base) {
+            std::is_same_v<decltype(T::classof(base)), bool>;
+        };
+
+        
     } // namespace coro
 } // namespace async_framework

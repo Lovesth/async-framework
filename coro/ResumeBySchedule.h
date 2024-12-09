d8e8e2052a1ea820ca360ccbebd7100e1f13e3aa
Lovesth <1667286082@qq.com>
2024-12-09 18:52:11 +0800

    commit

--- coro/ResumeBySchedule.h
+++ coro/ResumeBySchedule.h
@@ -13,9 +13,53 @@ namespace async_framework::coro
         template <typename T>
         class FutureResumeByScheduleAwaiter
         {
-            
+        public:
+            FutureResumeByScheduleAwaiter(Future<T> &&f) : future_(std::move(f)) {}
+            bool await_ready() { return future_.hasResult(); }
+
+            template <typename PromiseType>
+            void await_suspend(std::coroutine_handle<PromiseType> continuation)
+            {
+                static_assert(std::is_base_of_v<LazyPromiseBase, PromiseType>, "FutureResumeByScheduleAwaiter is only allowed to be called by Lazy");
+                Executor *ex = continuation.promise().executor_;
+                future_.setContinuation([continuation, ex](Try<T> &&t) mutable
+                                        {
+                    if(ex != nullptr){
+                        ex->schedule(continuation);
+                    }else{
+                        continuation.resume();
+                    } });
+            }
+            auto await_resume()
+            {
+                return std::move(future_.value());
+            }
+
+        private:
+            Future<T> future_;
+        };
+
+        template <typename T>
+        class FutureResumeByScheduleAwaitable
+        {
+        public:
+            explicit FutureResumeByScheduleAwaitable(Future<T> &&f) : future_(std::move(f))
+            {
+            }
+
+            auto coAwait(Executor *)
+            {
+                return FutureResumeByScheduleAwaiter(std::move(future_));
+            }
+
         private:
             Future<T> future_;
         };
     } // namespace detail
+
+    template <typename T>
+    inline auto ResumeBySchedule(Future<T> &&future)
+    {
+        return detail::FutureResumeByScheduleAwaitable<T>(std::move(future));
+    }
 } // namespace async_framework::coro
